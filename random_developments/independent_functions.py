4538f69ab49756e3b8ee5e41e81ca91fc248edaf
dbhusserl <dhusserl44@gmail.com>
2019-07-15 20:52:25 -0400

    D.Husserl - Adding informal development structure.

---
+++ random_developments/independent_functions.py
@@ -0,0 +1,8 @@
+#------------------------------------------------------------------------------------------------------#
+# Purpose: Provide a single .py file to house stand alone functions and classes for educational purposes
+
+# Do: Practice your development techniques and entertain new technologies
+# Do Not: Use this file as the framework for a production projects
+#------------------------------------------------------------------------------------------------------#
+
+class dylans_functions():
